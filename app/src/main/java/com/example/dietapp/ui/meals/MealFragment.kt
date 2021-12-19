e48d188083ba430f2ec0283f36bc0c0170ee8d9e
Kamil Donda <dondakamil@gmail.com>
2021-12-19 16:38:26 +0100

    Setup TabLayout and ViewPager2

--- app/src/main/java/com/example/dietapp/ui/meals/MealFragment.kt
+++ app/src/main/java/com/example/dietapp/ui/meals/MealFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.dietapp.R
+import com.example.dietapp.adapters.MealPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
-class MealFragment : Fragment() {
+class MealFragment : Fragment(), TabLayout.OnTabSelectedListener {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,4 +22,30 @@ class MealFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_meal, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tabLayout = view.findViewById(R.id.tabLayout)
+        tabLayout.addOnTabSelectedListener(this)
+
+        val manager = requireActivity().supportFragmentManager
+        val adapter = MealPagerAdapter(manager, lifecycle)
+
+        viewPager2 = view.findViewById(R.id.viewPager2)
+        viewPager2.adapter = adapter
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        viewPager2.currentItem = tab!!.position
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {}
+    override fun onTabReselected(tab: TabLayout.Tab?) {}
 }
---
+++ app/src/main/java/com/example/dietapp/ui/meals/PreparationOfMealFragment.kt
@@ -0,0 +1,19 @@
+package com.example.dietapp.ui.meals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.dietapp.R
+
+class PreparationOfMealFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_preparation_of_meal, container, false)
+    }
+}
