1962274d7db49fb166f33400fdaa23917a09412b
Kamil Donda <dondakamil@gmail.com>
2021-12-20 15:49:44 +0100

    Add pie chart and macronutrients

--- app/src/main/java/com/example/dietapp/ui/meals/IngredientsOfMealFragment.kt
+++ app/src/main/java/com/example/dietapp/ui/meals/IngredientsOfMealFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.dietapp.R
+import com.example.dietapp.adapters.MacronutrientsAdapter
+import com.example.dietapp.models.Meal
+import kotlinx.android.synthetic.main.fragment_ingredients_of_meal.*
+import org.eazegraph.lib.models.PieModel
 
 class IngredientsOfMealFragment : Fragment() {
+
+    private val macronutrientsAdapter = MacronutrientsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +22,42 @@ class IngredientsOfMealFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_ingredients_of_meal, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        macronutrients_rv.adapter = macronutrientsAdapter
+
+        val meal = Meal(1, "Name1", "", "", 3200f, 30f, 64.5f, 42.536f)
+
+        setData(meal)
+    }
+
+    private fun setData(meal: Meal) {
+        val macronutrients = arrayListOf(
+            Triple(
+                ContextCompat.getColor(requireContext(), R.color.colorProteins),
+                getString(R.string.proteins),
+                meal.proteins
+            ),
+            Triple(
+                ContextCompat.getColor(requireContext(), R.color.colorCarbs),
+                getString(R.string.carbs),
+                meal.carbs
+            ),
+            Triple(
+                ContextCompat.getColor(requireContext(), R.color.colorFats),
+                getString(R.string.fats),
+                meal.fats
+            )
+        )
+        macronutrientsAdapter.setList(macronutrients)
+
+        macronutrients.forEach {
+            macronutrients_pieChart.addPieSlice(PieModel(it.third, it.first))
+        }
+
+        macronutrients_pieChart.startAnimation()
     }
 }
