e3f1540934e4bc49bc95bb042860bc1d34ecbc1b
Kwolik <kwolikzsp6@gmail.com>
2021-12-26 14:16:31 +0100

    Configuration sign in for login and password

--- app/src/main/java/com/example/dietapp/ui/loginactivity/login/LoginFragment.kt
+++ app/src/main/java/com/example/dietapp/ui/loginactivity/login/LoginFragment.kt
@@ -10,12 +10,14 @@ import androidx.navigation.findNavController
 import com.example.dietapp.R
 import com.example.dietapp.database.sharedpreferences.Preferences
 import com.example.dietapp.ui.mainactivity.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_login.*
 import org.koin.android.ext.android.inject
 
 class LoginFragment : Fragment() {
 
     private val sharedPreferences: Preferences by inject()
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,14 +30,36 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        login_button.setOnClickListener {
-            val intent = Intent(requireContext(), MainActivity::class.java)
-            sharedPreferences.setIsLogged(true)
-            startActivity(intent)
-        }
-
+        setupSignInClick()
         login_back_button.setOnClickListener {
             it.findNavController().navigate(R.id.action_loginFragment_to_startFragment)
+        }
+    }
+
+    private fun setupSignInClick() {
+        login_button.setOnClickListener {
+            val email = email_input.text?.trim().toString()
+            val password = password_input.text?.trim().toString()
+
+            when {
+//                email.isEmpty() -> showSnackbar(getString(R.string.email_is_empty))
+//                password.isEmpty() -> showSnackbar(getString(R.string.password_is_empty))
+                else -> {
+                    auth.signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener {
+                            if (it.user != null)
+                            {
+                                val intent = Intent(requireContext(), MainActivity::class.java)
+                                sharedPreferences.setIsLogged(true)
+                                startActivity(intent)
+                            }
+                        }
+                        .addOnFailureListener {
+                            //showSnackbar(it.message.toString())
+                        }
+                }
+            }
+
         }
     }
 }
