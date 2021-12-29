d4ecf45872a70a947d777358e7c1f97251b21215
Kwolik <kwolikzsp6@gmail.com>
2021-12-29 21:56:01 +0100

    Configuration third chart -  bar macro

--- app/src/main/java/com/example/dietapp/ui/mainactivity/home/DayFragment.kt
+++ app/src/main/java/com/example/dietapp/ui/mainactivity/home/DayFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.dietapp.R
@@ -12,8 +13,16 @@ import com.example.dietapp.adapters.MacroDayAdapter
 import com.example.dietapp.adapters.StaticDayAdapter
 import com.example.dietapp.models.Macroday
 import com.example.dietapp.models.Static
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import kotlinx.android.synthetic.main.fragment_day.*
 import org.eazegraph.lib.models.PieModel
+import java.util.*
+
 
 class DayFragment : Fragment() {
 
@@ -36,10 +45,12 @@ class DayFragment : Fragment() {
 
         setStatisticsData(Static(1, 840f, 1280f, 640f))
         setMacroData(Macroday(1, 245f, 124f, 368f))
+        lol()
 
         day_back_button.setOnClickListener {
             it.findNavController().navigate(R.id.action_dayFragment_to_homeFragment)
         }
+
     }
 
     private fun setStatisticsData(static: Static) {
@@ -94,5 +105,110 @@ class DayFragment : Fragment() {
         }
 
         macro_pieChart.startAnimation()
+    }
+
+    private fun getBarProteins(): ArrayList<BarEntry> {
+
+        val barEntries = ArrayList<BarEntry>()
+
+        barEntries.add(BarEntry(1f, 14.5f))
+        barEntries.add(BarEntry(2f, 12.5f))
+        barEntries.add(BarEntry(3f, 6.2f))
+        return barEntries
+    }
+
+    private fun getBarCarbs(): ArrayList<BarEntry> {
+
+        val barEntries = ArrayList<BarEntry>()
+
+        barEntries.add(BarEntry(1f, 8.5f))
+        barEntries.add(BarEntry(2f, 4.2f))
+        barEntries.add(BarEntry(3f, 7.8f))
+        return barEntries
+    }
+
+    private fun getBarFats(): ArrayList<BarEntry> {
+
+        val barEntries = ArrayList<BarEntry>()
+
+        barEntries.add(BarEntry(1f, 6.8f))
+        barEntries.add(BarEntry(2f, 11.1f))
+        barEntries.add(BarEntry(3f, 7.2f))
+        return barEntries
+    }
+
+    private fun lol()
+    {
+        //Tworzenie zmiennych do wykresu
+        val barChart = idBarChart
+        val barDataSet1: BarDataSet
+        val barDataSet2: BarDataSet
+        val barDataSet3: BarDataSet
+        val days = arrayOf(
+            getString(R.string.breakfast),
+            getString(R.string.lunch),
+            getString(R.string.dinner)
+        )
+
+        //Inicjacja danych, kolorów, wartości nad wykresami
+        barDataSet1 = BarDataSet(getBarProteins(), getString(R.string.proteins))
+        barDataSet1.setColor(getResources().getColor(R.color.colorProteins))
+        barDataSet1.setDrawValues(true)
+        barDataSet1.valueTextSize = 12f
+        barDataSet2 = BarDataSet(getBarCarbs(), getString(R.string.carbs))
+        barDataSet2.setColor(getResources().getColor(R.color.colorCarbs))
+        barDataSet2.setDrawValues(true)
+        barDataSet2.valueTextSize = 12f
+        barDataSet3 = BarDataSet(getBarFats(), getString(R.string.fats))
+        barDataSet3.setColor(getResources().getColor(R.color.colorFats))
+        barDataSet3.setDrawValues(true)
+        barDataSet3.valueTextSize = 12f
+
+        //Stworzenie zmiennej data ze wszystkimi danymi do wykresu
+        val data = BarData(barDataSet1, barDataSet2, barDataSet3)
+
+        //Stworzenie zmiennej dla opisu osi X i legendy
+        val xAxis = barChart.xAxis
+        val legend: Legend = barChart.getLegend()
+
+        barChart.data = data
+        barChart.description.isEnabled = false
+        barChart.isDragEnabled = true
+        barChart.setVisibleXRangeMaximum(3f)
+//        barChart.setDoubleTapToZoomEnabled(false)
+//        barChart.setPinchZoom(false)
+//        barChart.setScaleEnabled(false)
+
+        legend.isEnabled = true
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.textSize = 14f
+        legend.xEntrySpace = 22f
+        legend.xOffset = 24f
+        legend.yOffset = 12f
+
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(true)
+        xAxis.setDrawAxisLine(true)
+        xAxis.setCenterAxisLabels(true)
+        xAxis.axisMinimum = 0f
+        xAxis.granularity = 1.245f  //Przesuwanie opisu osi X (śniadanie, obiad, kolacja)
+        xAxis.setTextSize(14f)
+        xAxis.setAxisMaximum(data.getXMax() + 0.74f)
+        xAxis.valueFormatter = IndexAxisValueFormatter(days)
+        xAxis.isGranularityEnabled = true
+        xAxis.yOffset = -1.6f
+
+        val barSpace = 0.2f
+        val groupSpace = 0.12f
+        data.barWidth = 0.18f
+
+        barChart.animate()
+
+        barChart.groupBars(0f, groupSpace, barSpace)
+
+        barChart.animateXY(2000, 2000)
+        barChart.invalidate()
     }
 }
